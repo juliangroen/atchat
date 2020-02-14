e56d5e6ce2c76892cf1d9e2da8a460ba362f73a7
Akufoolz <akufoolz@gmail.com>
2020-02-14 12:48:29 -0700

    implemental sign up modal click events

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const mainIconMenu = document.querySelector('#mainIconMenu');
+const mainLinkSignUp = document.querySelector('#mainLinkSignUp');
+const mainButtonSignUp = document.querySelector('#mainButtonSignUp');
 const menuIconClose = document.querySelector('#menuIconClose');
 const menuLinkSignUp = document.querySelector('#menuLinkSignUp');
 const modalBackground = document.querySelector('#modalBackground');
 const modalForeground = document.querySelector('#modalForeground');
 const modalIconClose = document.querySelector('#modalIconClose');
 const mobileMenu = document.querySelector('#mobileMenu');
+
+const openModalSignUp = function() {
+  mobileMenu.classList.add('hidden');
+  modalBackground.classList.remove('hidden');
+  modalForeground.classList.remove('hidden');
+};
+
+const closeModalSignUp = function() {
+  modalBackground.classList.add('hidden');
+  modalForeground.classList.add('hidden');
+};
 
 mainIconMenu.addEventListener('click', function() {
   mobileMenu.classList.remove('hidden');
@@ -15,12 +28,17 @@ menuIconClose.addEventListener('click', function() {
 });
 
 menuLinkSignUp.addEventListener('click', function() {
-  mobileMenu.classList.add('hidden');
-  modalBackground.classList.remove('hidden');
-  modalForeground.classList.remove('hidden');
+  openModalSignUp();
+});
+
+mainLinkSignUp.addEventListener('click', function() {
+  openModalSignUp();
+});
+
+mainButtonSignUp.addEventListener('click', function() {
+  openModalSignUp();
 });
 
 modalIconClose.addEventListener('click', function() {
-  modalBackground.classList.add('hidden');
-  modalForeground.classList.add('hidden');
+  closeModalSignUp();
 });
